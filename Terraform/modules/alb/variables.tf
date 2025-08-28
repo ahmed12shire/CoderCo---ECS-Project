36ba75cb111ed186ac5b0edd6cec8365b1dfd234
ahmed12shire <ahmed12shire@gmail.com>
2025-08-28 18:13:04 +0100

    final ECS project test2

---
+++ Terraform/modules/alb/variables.tf
@@ -0,0 +1,10 @@
+
+variable "vpc_id" {
+  description = "VPC ID where the ALB will be deployed"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs for the ALB"
+  type        = list(string)
+}
