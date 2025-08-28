f6ecc68a4817b731ace645f82e98b21743577fe0
ahmed12shire <ahmed12shire@gmail.com>
2025-08-28 21:18:56 +0100

    final ECS project test12

--- Terraform/modules/route53/main.tf
+++ Terraform/modules/route53/main.tf
@@ -7,7 +7,7 @@ resource "aws_route53_zone" "main" {
 }
 
 resource "aws_route53_record" "alb_record" {
-  zone_id = var.app_lb_zone_id
+  zone_id = aws_route53_zone.main.zone_id 
   name    = var.domain_name
   type    = "A"
 
