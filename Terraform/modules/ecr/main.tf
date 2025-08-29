1a03c52ac9bbd68d0a0a3f5590e73c37b3c3d007
ahmed12shire <ahmed12shire@gmail.com>
2025-08-29 17:44:38 +0100

    destroy2

--- Terraform/modules/ecr/main.tf
+++ Terraform/modules/ecr/main.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "ecs_repo" {
-  name = var.repository_name
+  name         = var.repository_name
+  force_delete = true
 
   tags = merge(local.common_tags, {
     Name = "${var.project_name}-repo"
