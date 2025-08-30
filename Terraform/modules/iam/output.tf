4447df1a5ec5afd208a8aa4ebe767b662ff4e349
ahmed12shire <ahmed12shire@gmail.com>
2025-08-30 12:45:31 +0100

    ecs-project doplyment final 2

--- Terraform/modules/iam/output.tf
+++ Terraform/modules/iam/output.tf
@@ -5,3 +5,8 @@ output "ecs_execution_role_arn" {
 output "ecs_task_role_arn" {
   value = aws_iam_role.ecs_task_role.arn
 }
+
+
+output "ecs_instance_role_name" {
+  value = aws_iam_role.ecs_instance_role.name
+}
