cd0dbf51651366b49db01c811b0c2cdd6b929944
ahmed12shire <ahmed12shire@gmail.com>
2025-08-30 12:39:18 +0100

    ecs-project doplyment final 1

--- Terraform/modules/compute/main.tf
+++ Terraform/modules/compute/main.tf
@@ -5,6 +5,8 @@ resource "aws_instance" "ecsproject" {
   subnet_id     = element(var.subnet_id, count.index % length(var.subnet_id))
   vpc_security_group_ids = var.vpc_security_group_ids
 
+  iam_instance_profile = aws_iam_role.ecs_instance_role.name
+
 
   
   tags = merge(
--- Terraform/modules/iam/main.tf
+++ Terraform/modules/iam/main.tf
@@ -8,12 +8,46 @@ data "aws_iam_policy_document" "ecs_assume_role_policy" {
   }
 }
 
+# ECS Execution Role (for ECS to pull images and push logs)
 resource "aws_iam_role" "ecs_execution_role" {
   name               = "ecs-execution-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_execution_role.name
+}
+
+# ECS Task Role (for ECS tasks to interact with AWS service
 resource "aws_iam_role" "ecs_task_role" {
   name               = "ecs-task-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceTaskRolePolicy"
+  role       = aws_iam_role.ecs_task_role.name
+}
+
+# EC2 Instance Role (for EC2 instances to register with ECS)
+resource "aws_iam_role" "ecs_instance_role" {
+  name               = "ecsInstanceRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Effect    = "Allow"
+    }]
+  })
+}
+
+# Attach policy to EC2 Instance Role for ECS agent interaction
+resource "aws_iam_role_policy_attachment" "ecs_instance_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+  role       = aws_iam_role.ecs_instance_role.name
+}
+
+
+
